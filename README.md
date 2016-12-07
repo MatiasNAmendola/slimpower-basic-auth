a110b36265b877549e30432d1477528f7fbe0a8b
Matias Nahuel Améndola <soporte.esolutions@gmail.com>
2016-12-07 01:58:05 -0300

    v0.0.1-alpha

---
+++ README.md
@@ -0,0 +1,52 @@
+#SlimPower - HTTP Basic Authentication
+
+[![Latest version][ico-version]][link-packagist]
+[comment]: # ([![Total Downloads][ico-downloads]][link-downloads])
+
+[![Latest Stable Version](https://poser.pugx.org/matiasnamendola/slimpower-basic-auth/version?format=flat-square)](https://packagist.org/packages/matiasnamendola/slimpower-basic-auth) 
+[![Latest Unstable Version](https://poser.pugx.org/matiasnamendola/slimpower-basic-auth/v/unstable?format=flat-square)](//packagist.org/packages/matiasnamendola/slimpower-basic-auth) 
+[![Total Downloads](https://poser.pugx.org/matiasnamendola/slimpower-basic-auth/downloads?format=flat-square)](https://packagist.org/packages/matiasnamendola/slimpower-basic-auth) 
+[![Monthly Downloads](https://poser.pugx.org/matiasnamendola/slimpower-basic-auth/d/monthly?format=flat-square)](https://packagist.org/packages/matiasnamendola/slimpower-basic-auth)
+[![Daily Downloads](https://poser.pugx.org/matiasnamendola/slimpower-basic-auth/d/daily?format=flat-square)](https://packagist.org/packages/matiasnamendola/slimpower-basic-auth)
+[![composer.lock available](https://poser.pugx.org/matiasnamendola/slimpower-basic-auth/composerlock?format=flat-square)](https://packagist.org/packages/matiasnamendola/slimpower-basic-auth)
+
+HTTP Basic Authentication Middleware for SlimPower Framework
+
+##Installation
+
+In terminal:
+
+```sh
+composer require matiasnamendola/slimpower-basic-auth
+```
+
+Or you can add use this as your composer.json:
+
+```json
+{
+    "require": {
+        "matiasnamendola/slimpower-basic-auth": "dev-master"
+    }
+}
+```
+
+##Security
+
+If you discover any security related issues, please email [soporte.esolutions@gmail.com](mailto:soporte.esolutions@gmail.com?subject=[SECURITY] Config Security Issue) instead of using the issue tracker.
+
+
+##Credits
+
+- [Matías Nahuel Améndola](https://github.com/matiasnamendola)
+- [Franco Soto](https://github.com/francosoto)
+
+
+##License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/MatiasNAmendola/slimpower-basic-auth.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/MatiasNAmendola/slimpower-basic-auth.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/matiasnamendola/slimpower-basic-auth
+[link-downloads]: https://packagist.org/packages/matiasnamendola/slimpower-basic-auth
---
+++ src/HttpBasicAuthentication/PdoAuthenticator.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This file is part of Slim HTTP Basic Authentication middleware
+ *
+ * @package    SlimPower
+ * @subpackage BasicAuth
+ * @author     Matias Nahuel Améndola <soporte.esolutions@gmail.com>
+ * @link       https://github.com/MatiasNAmendola/slimpower-basic-auth
+ * @license    https://github.com/MatiasNAmendola/slimpower-basic-auth/blob/master/LICENSE.md
+ * @since      0.0.1
+ * 
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace SlimPower\BasicAuth\HttpBasicAuthentication;
+
+class PdoAuthenticator implements AuthenticatorInterface {
+
+    private $options;
+
+    public function __construct(array $options = array()) {
+
+        /* Default options. */
+        $this->options = array(
+            "table" => "users",
+            "user" => "user",
+            "hash" => "hash"
+        );
+
+        if ($options) {
+            $this->options = array_merge($this->options, $options);
+        }
+    }
+
+    public function __invoke(array $arguments) {
+        $user = $arguments["user"];
+        $password = $arguments["password"];
+
+        $driver = $this->options["pdo"]->getAttribute(\PDO::ATTR_DRIVER_NAME);
+
+        $sql = $this->sql();
+
+        $statement = $this->options["pdo"]->prepare($sql);
+        $statement->execute(array($user));
+
+        if ($user = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            return password_verify($password, $user[$this->options["hash"]]);
+        }
+
+        return false;
+    }
+
+    public function sql() {
+        $driver = $this->options["pdo"]->getAttribute(\PDO::ATTR_DRIVER_NAME);
+
+        /* Workaround to test without sqlsrv with Travis */
+        if (defined("__PHPUNIT_ATTR_DRIVER_NAME__")) {
+            $driver = __PHPUNIT_ATTR_DRIVER_NAME__;
+        }
+
+        if ("sqlsrv" === $driver) {
+            $sql = "SELECT TOP 1 *
+                 FROM {$this->options['table']}
+                 WHERE {$this->options['user']} = ?";
+        } else {
+            $sql = "SELECT *
+                 FROM {$this->options['table']}
+                 WHERE {$this->options['user']} = ?
+                 LIMIT 1";
+        }
+
+        return preg_replace("!\s+!", " ", $sql);
+    }
+
+}
