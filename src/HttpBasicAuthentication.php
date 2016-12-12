5d396949ef3838e94a1fa1c70828650eed2a8de9
Matias Nahuel Améndola <soporte.esolutions@gmail.com>
2016-12-12 00:50:35 -0300

    v0.0.1-alpha

--- src/HttpBasicAuthentication.php
+++ src/HttpBasicAuthentication.php
@@ -36,6 +36,7 @@
 namespace SlimPower\HttpBasicAuthentication;
 
 use SlimPower\Authentication\AbstractAuthentication;
+use SlimPower\Authentication\ArrayAuthenticator;
 
 class HttpBasicAuthentication extends AbstractAuthentication {
 
